e34c12d416b7e1573f8ae50542895bea88b7a644
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2021-09-06 15:00:03 -0300

    Inclusão da classe App

---
+++ atvi-wb/src/main/java/com/wb/App.java
@@ -0,0 +1,39 @@
+package com.wb;
+
+import com.wb.io.Entrada;
+import com.wb.modelo.Empresa;
+import com.wb.negocio.Cadastro;
+import com.wb.negocio.CadastroCliente;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Bem-vindo ao cadastro de clientes do Grupo World Beauty");
+		Empresa empresa = new Empresa();
+		boolean execucao = true;
+		while (execucao) {
+			System.out.println("Que tipo de operação você deseja fazer:");
+			System.out.println("1 - Cadastrar cliente");
+			System.out.println("2 - Listar todos os clientes");
+			System.out.println("0 - Sair");
+
+			Entrada entrada = new Entrada();
+			int operacao = entrada.receberNumeroInteiro();
+
+			switch (operacao) {
+			case 0:
+				execucao = false;
+				System.out.println("Até mais!\n");
+				break;
+			case 1:
+				Cadastro cadastro = new CadastroCliente(empresa.getClientes());
+				cadastro.cadastrar();
+				break;
+			case 2:
+				System.out.println("nada por hora");
+				break;
+			default:
+				System.out.println("Operação não entendida");
+			}
+		}
+	}
+}
