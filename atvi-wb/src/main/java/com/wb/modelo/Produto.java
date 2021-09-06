76b03531d7e9ebf133849e67c46855bdef0748e9
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2021-09-06 14:14:07 -0300

    Inclusão das classes de modelo

---
+++ atvi-wb/src/main/java/com/wb/modelo/Produto.java
@@ -0,0 +1,5 @@
+package com.wb.modelo;
+
+public abstract class Produto {
+	public String nome;
+}
---
+++ atvi-wb/src/main/java/com/wb/modelo/Servico.java
@@ -0,0 +1,5 @@
+package com.wb.modelo;
+
+public abstract class Servico {
+	public String nome;
+}
